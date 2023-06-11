a518b3f6c8be7bece71e972aeaa2de59ec385384
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-06-12 03:18:58 +0900

    Add: JavaScript로 DOM 조작하기
    DOM이란 다큐먼트 오브젝트 모델이라 해서 HTML과 CSS를 조작하는 걸 얘기합니다.

--- javascript-practice/app.js
+++ javascript-practice/app.js
@@ -1,42 +1,11 @@
-// 함수에 대한 같은 표현
-// const 더하기 = (a, b) => {
-//   console.log("가격은", a + b, "만원입니다.");
-// };
+// 태그를 가져오는 방법
+// const timeElement = document.getElementById("time");
+// 태그를 가져오는 다른 방법
+// const timeElement = document.querySelector("h1");
+const timeElement = document.querySelector("#time");
 
-// console.log("애플워치랑 맥북 가격이 얼마에요?");
-// 더하기(50, 200);
+timeElement.style.color = "tomato";
+timeElement.style.backgroundColor = "orange";
+timeElement.innerText = "12:00";
 
-// 계산기 만들기 v.2
-// const 계산기 = {
-//   더하기: function (a, b) {
-//     return a + b;
-//   },
-//   빼기: function (a, b) {
-//     return a - b;
-//   },
-//   곱하기: (a, b) => {
-//     return a * b;
-//   },
-//   나누기: (a, b) => {
-//     return a / b;
-//   },
-// };
-
-// console.log(계산기.더하기(1, 2));
-// console.log(계산기.빼기(10, 2));
-// console.log(계산기.곱하기(10, 2));
-// const 나누기_된_값 = 계산기.나누기(500, 2);
-// console.log(나누기_된_값);
-
-// function 할인된_가격(가격) {
-//   if (가격 > 10000) return 가격 - 1000;
-//   else return 가격;
-// }
-
-// console.log(할인된_가격(15000));
-// console.log(할인된_가격(5000));
-
-// 반복문
-for (let i = 0; i < 10; i++) {
-  console.log("이건 얼마냐구요!!");
-}
+document.title = "❤❤❤❤❤❤❤";
