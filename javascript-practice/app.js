9b058a2365b9d1fe7733270aceb41c47d218eaf4
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-06-12 06:15:38 +0900

    Update: 이벤트를 넣는 방법
    특정 엘레멘트에 이벤트를 줄 수 있고. 전체, 윈도우 자체에도 이벤트를 줄 수 있습니다.
    style을 응용해서 if문을 통해 이벤트를 줄 수도 있습니다.

--- javascript-practice/app.js
+++ javascript-practice/app.js
@@ -1,11 +1,40 @@
-// 태그를 가져오는 방법
-// const timeElement = document.getElementById("time");
-// 태그를 가져오는 다른 방법
-// const timeElement = document.querySelector("h1");
+// 이벤트를 넣는 방법
 const timeElement = document.querySelector("#time");
 
-timeElement.style.color = "tomato";
-timeElement.style.backgroundColor = "orange";
-timeElement.innerText = "12:00";
+// function 클릭시_실행될_함수() {
+//   timeElement.style.color = "#fff";
+//   timeElement.style.backgroundColor = "pink";
+//   timeElement.innerText = "12:00";
+// }
 
-document.title = "❤❤❤❤❤❤❤";
+// 주의할 점은 클릭시_실행된_함수에 괄호를 쳐서 호출하면 바로 실행되버림.
+// timeElement.addEventListener("click", 클릭시_실행될_함수);
+// timeElement.addEventListener("mouseover", 실행될_함수);
+
+// 특정 엘레멘트에 이벤트를 줄 수도 있지만 전체, 윈도우 자체에도 이벤트를 줄 수 있어요.
+// function 실행될_함수() {
+//   timeElement.style.color = "red";
+//   timeElement.style.backgroundColor = "rgb(0, 118, 189)";
+//   timeElement.innerText = "24:00";
+// }
+
+// 복사 버튼이나 단축키 누를 때 이벤트 실행
+// window.addEventListener("copy", 실행될_함수);
+// 브라우저 창 크기를 조절할 때 이벤트 실행
+// window.addEventListener("resize", 실행될_함수);
+
+// 이번에는 style을 응용해서 윈도우가 아닌 엘레멘트에 이벤트 주기
+function 색깔을_바꿔주는_함수() {
+  if (timeElement.style.color === "orange") {
+    timeElement.style.color = "blue";
+  } else {
+    timeElement.style.color = "orange";
+  }
+}
+
+function 숫자를_바꿔주는_함수() {
+  timeElement.innerText = "12:00";
+}
+
+timeElement.addEventListener("click", 색깔을_바꿔주는_함수);
+timeElement.addEventListener("click", 숫자를_바꿔주는_함수);
