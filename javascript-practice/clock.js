cd3125438e3679db946bb2624c1ad2ab8e4e8bbd
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-06-14 05:58:41 +0900

    Update: 타이머만들기
    현재 시간과 흐른 시간을 비교해서 타이머 완성.

--- javascript-practice/clock.js
+++ javascript-practice/clock.js
@@ -1,9 +1,13 @@
+// 타이머 만들기
+const 시작_시간 = new Date();
+
 function setTime() {
-  const time = new Date();
-  const 분 = time.getMinutes().toString();
-  const 초 = time.getSeconds().toString();
+  const 현재_시간 = new Date();
+  const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+  const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+  const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
   const timeH1 = document.querySelector(".time");
-  timeH1.innerText = `${분.padStart(2, "0")}:${초.padStart(2, "0")}`;
+  timeH1.innerText = `${분}:${초}`;
 }
 
 // 주기성
