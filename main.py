69f37b5597f01eb142324f6d170054097ef3fc8d
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-06-20 10:23:32 +0900

    Update: FastAPI로 간단한 API 서버 만들기

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/hello")
+async def sayHello():
+    return {"message": "안녕하세요 슈퍼코딩!"}
+
+@app.get("/")
+def sayWelcome():
+    return {"message":"환영합니다!!"}
