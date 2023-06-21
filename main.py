c9b34d8136fb0e961e2a331bb145a32a941d3584
laurenCho9 <lauren.choux@gmail.com>
2023-06-21 22:05:10 +0900

    Update: 백엔드에게 정보를 보내는 방식
    1. Path parameter
    2. Query parameter
    3. Request Body

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # main.py
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/hello")
-async def sayHello():
-    return {"message": "안녕하세요 슈퍼코딩!"}
+class Item(BaseModel):
+    id:int
+    content:str
 
-@app.get("/")
-def sayWelcome():
-    return {"message":"환영합니다!!"}
+items = ["맥북", "애플워치", "아이폰", "에어팟"]
+
+@app.get('/items/{id}')
+def read_id_item(id):
+    return items[int(id)] 
+
+@app.get('/items')
+def read_item(skip:int=0,limit:int=10):
+    return items[skip:skip+limit]
+
+@app.post("/items")
+def post_item(item:Item):
+    items.append(item.content)
+    return '성공했습니다!!'
